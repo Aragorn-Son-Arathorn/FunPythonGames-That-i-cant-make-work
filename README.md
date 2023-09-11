938f790c04e2800072f6c9353fecb10f4ef4a1e7
Aragorn-Son-Arathorn <sendderekspamhere@gmail.com>
2023-09-11 17:38:30 -0400

    committed!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FunPythonGames-That-i-cant-make-work
+
+# I'm doing this  as a way to help myself learn to code better and stuff. 
---
+++ SonarTreasureHunt.py
@@ -0,0 +1,20 @@
+#This is a game from the no starch book Invent your own computer games with python
+#Hopefully I start understanding what i'm actually typing and stuff as i go through it
+
+import random
+import sys
+import math
+
+#First gonna creat a board you'll use whenever you play.
+def getNewBoard():
+    board = []
+    for x in range(60): #the main list is a list of 60 lists this will be the x axis on the board where it goes 0-9 5 times
+        board.append([])
+        for y in range(15): #each list is the main list has 15 single-character strings this will be the y-axis on the board where it goes 0-14
+            if random.randint(0, 1) == 0: #this if/else statement will help draw the board as an ocean.
+                board[x].append('~')
+            else:
+                board[x].append('`')
+    return board
+
+
